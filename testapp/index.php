7fa57d698659a85520b5ff12a75260bd73191146
Christian24GH <loquezchristian@gmail.com>
2025-04-16 19:16:41 +0800

    fix

--- testapp/index.php
+++ testapp/index.php
@@ -9,7 +9,7 @@
     <title>TEST APP</title>
 </head>
 <body>
-    <form action="./session.php" method="POST">
+    <form method="POST">
         <button type="submit" name="logout">Logout</button>
     </form>
 </body>
